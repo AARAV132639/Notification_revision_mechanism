9be481dc162574894d8dadba3cea5279582a9a14
AARAV132639 <aaravkumar2809@gmail.com>
2026-03-26 18:33:12 +0530

    Version 0.0. Currently the notification works within the app only

---
+++ app/src/main/java/com/example/notification_revision/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.notification_revision
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.notification_revision.ui.theme.Notification_revisionTheme
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        createNotificationChannel()
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU) {
+            requestPermissions(arrayOf(android.Manifest.permission.POST_NOTIFICATIONS), 1)
+        }
+
+        sendTestNotification()
+    }
+
+    private fun sendTestNotification()
+    {
+        val channelId= "revision_channel"
+        val builder= NotificationCompat.Builder(this,channelId)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentText("What is normalization?")
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+
+        val manager= NotificationManagerCompat.from(this)
+        manager.notify(1,builder.build())
+    }
+
+    private fun createNotificationChannel() {
+        val channelId = "revision_channel"
+        val name = "Revision Notifications"
+        val descriptionText = "Channel for revision questions"
+        val importance = NotificationManager.IMPORTANCE_HIGH
+
+        val channel = NotificationChannel(channelId, name, importance).apply {
+            description = descriptionText
+        }
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        notificationManager.createNotificationChannel(channel)
+    }
+}
+
